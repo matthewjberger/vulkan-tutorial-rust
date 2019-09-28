271e645c252065a69ea86ef6b1a9098c821dcead
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-09-28 16:06:32 -0700

    Add linux platform support

--- src/platform.rs
+++ src/platform.rs
@@ -3,11 +3,23 @@ use ash::extensions::{DebugReport, Surface};
 #[cfg(target_os = "windows")]
 use ash::extensions::Win32Surface;
 
-#[cfg(all(windows))]
+#[cfg(target_os = "linux")]
+use ash::extensions::XlibSurface;
+
+#[cfg(target_os = "windows")]
 pub fn required_extension_names() -> Vec<*const i8> {
     vec![
         Surface::name().as_ptr(),
         Win32Surface::name().as_ptr(),
+        DebugReport::name().as_ptr(),
+    ]
+}
+
+#[cfg(target_os = "linux")]
+pub fn required_extension_names() -> Vec<*const i8> {
+    vec![
+        Surface::name().as_ptr(),
+        XlibSurface::name().as_ptr(),
         DebugReport::name().as_ptr(),
     ]
 }
